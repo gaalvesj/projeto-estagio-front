63d6cae4d7711983c13649992fc3de8dbc62df0c
Gabriel <gabrielalves3654@gmail.com>
2023-06-21 20:17:58 -0300

    fetch feito e verificacao de status code feito

---
+++ my-react-app/src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+import FetchDataComponent from './fetch/fetchDataComponent';
+
+function App() {
+  
+  return (
+    <div className="App">
+      <header className="App-header">
+        <FetchDataComponent />
+      </header>
+    </div>
+  );
+}
+
+export default App;
