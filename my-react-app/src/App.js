951d999532d888dac82c479b875a12dca3c15e89
Gabriel <gabrielalves3654@gmail.com>
2023-06-22 12:03:04 -0300

    adicionando genre na requsicao e colocando thumb

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,14 +1,50 @@
 
 import './App.css';
-import FetchDataComponent from './fetch/fetchDataComponent';
+import React, { useEffect, useState } from 'react';
+
 
 function App() {
-  
+  const [data, setData] = useState(null);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const URL = 'https://games-test-api-81e9fb0d564a.herokuapp.com/api/data/'
+    const codeVerify = [500, 502, 503, 504, 507, 508, 509]
+    fetch( URL, {
+      headers: {
+        'dev-email-address': 'seuemail@exemplo.com'
+      }
+    })
+    .then(response => {
+      if (codeVerify.includes(response.status)) {
+        console.log(response.status);
+        setError('O servidor falhou em responder, tente recarregar a página');
+        return Error;
+      }
+      return response.json();
+    })
+    .then(data => {
+      if (data === 'error') {
+        setData('O servidor não conseguirá responder por agora, tente voltar novamente mais tarde')
+      }
+      setData(data);
+      console.log(data)
+
+    })
+    .catch(() => setError('O servidor não conseguirá responder por agora, tente voltar novamente mais tarde'));
+  }, [])
+  if (error) {
+    return <div>O servidor falhou em responder, tente recarregar a página</div>;
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <FetchDataComponent />
-      </header>
+    <div>
+       {data ? data.map((item, index) => (
+        <div key={index}>
+          <h2>{item.title}</h2>
+          <p>{item.genre}</p>
+          <img src={item.thumbnail} alt={item.title} />
+        </div>
+      )) : 'loading...'}
     </div>
   );
 }
