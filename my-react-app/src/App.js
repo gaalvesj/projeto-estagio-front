09f58e4cb121bf403e0897628eb954ebe1a276bd
Gabriel <gabrielalves3654@gmail.com>
2023-06-22 13:11:31 -0300

    adicionando barra de pesquisa

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -2,10 +2,23 @@
 import './App.css';
 import React, { useEffect, useState } from 'react';
 
+function SearchBar({ onSearch }) {
+  const [search, setSearch] = useState('');
+
+  const handleSearchChange = (event) => {
+    setSearch(event.target.value);
+    onSearch(event.target.value);
+  };
+
+  return (
+    <input type="text" value={search} onChange={handleSearchChange} placeholder="Buscar por título..." />
+  );
+}
 
 function App() {
-  const [data, setData] = useState(null);
+  const[data, setData] = useState(null);
   const [error, setError] = useState(null);
+  const [search, setSearch] = useState('');
 
   useEffect(() => {
     const URL = 'https://games-test-api-81e9fb0d564a.herokuapp.com/api/data/'
@@ -36,15 +49,18 @@ function App() {
   if (error) {
     return <div>O servidor falhou em responder, tente recarregar a página</div>;
   }
+
+  const filteredData = data ? data.filter(item => item.title.toLowerCase().includes(search.toLowerCase())) : [];
+
   return (
-    <div className='app'>
-       {data ? data.map((item, index) => (
-        <div key={index} className='card'>
+    <div>
+      <SearchBar onSearch={setSearch} />
+      {filteredData.map((item, index) => (
+        <div key={index}>
           <h2>{item.title}</h2>
-          <p>{item.genre}</p>
           <img src={item.thumbnail} alt={item.title} />
         </div>
-      )) : 'loading...'}
+      ))}
     </div>
   );
 }
